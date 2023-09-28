c76bd843b6990599f3c4864c47240cff6eba6bbf
Alan Hamlett <alan.hamlett@gmail.com>
2023-09-28 12:09:29 +0200

    implement custom json serializer

--- src/serializer.ts
+++ src/serializer.ts
@@ -1,87 +1,4 @@
-/*
-class CustomJSONEncoder(JSONEncoder):
-    def __init__(self, *args, allow_nan=False, **kwargs):
-        kwargs["allow_nan"] = allow_nan
-        super().__init__(*args, **kwargs)
-
-    def default(self, o):
-        if isinstance(o, set):
-            return list(o)
-        if isinstance(o, Decimal):
-            return {
-                "__class__": "Decimal",
-                "value": str(o),
-            }
-        if isinstance(o, datetime):
-            if o.tzinfo is not None:
-                # tasks always receive datetimes in utc without tzinfo
-                o = o.astimezone(timezone.utc)
-            return {
-                "__class__": "datetime",
-                "year": o.year,
-                "month": o.month,
-                "day": o.day,
-                "hour": o.hour,
-                "minute": o.minute,
-                "second": o.second,
-                "microsecond": o.microsecond,
-                "fold": o.fold,
-            }
-        if isinstance(o, date):
-            return {
-                "__class__": "date",
-                "year": o.year,
-                "month": o.month,
-                "day": o.day,
-            }
-        if isinstance(o, timedelta):
-            return {
-                "__class__": "timedelta",
-                "kwargs": {
-                    "days": o.days,
-                    "seconds": o.seconds,
-                    "microseconds": o.microseconds,
-                },
-            }
-        if isinstance(o, bytes):
-            return {
-                "__class__": "bytes",
-                "value": b64encode(o).decode("ascii"),
-            }
-        return str(o)
-
-
-def object_hook(obj):
-    cls = obj.get("__class__")
-    if not cls:
-        return obj
-
-    if cls == "Decimal":
-        return Decimal(obj["value"])
-    if cls == "datetime":
-        return datetime(
-            year=obj["year"],
-            month=obj["month"],
-            day=obj["day"],
-            hour=obj["hour"],
-            minute=obj["minute"],
-            second=obj["second"],
-            microsecond=obj["microsecond"],
-            fold=obj["fold"],
-        )
-    if cls == "date":
-        return date(
-            year=obj["year"],
-            month=obj["month"],
-            day=obj["day"],
-        )
-    if cls == "timedelta":
-        return timedelta(**obj["kwargs"])
-    if cls == "bytes":
-        return b64decode(obj["value"])
-
-    return obj
-*/
+import { Duration } from 'ts-duration';
 
 function replacer(key: string, v: any): string | number | boolean | null {
   // @ts-ignore: Object is possibly 'null'
@@ -94,6 +11,17 @@ function replacer(key: string, v: any): string | number | boolean | null {
     case 'number':
       return v;
     case 'object':
+      if (orig instanceof Date) {
+        return JSON.stringify({
+          __class__: 'Date',
+          iso: orig.toISOString(),
+        });
+      } else if (orig instanceof Duration) {
+        return JSON.stringify({
+          __class__: 'Duration',
+          nanoseconds: orig.nanoseconds,
+        });
+      }
       return v;
     case 'string':
       return JSON.stringify(v);
@@ -104,6 +32,7 @@ function replacer(key: string, v: any): string | number | boolean | null {
 }
 
 function reviver(key: string, value: any): any {
+  // TODO: deserialize Date, Duration, and bigint
   if (typeof value === 'object') {
     return value;
   }
