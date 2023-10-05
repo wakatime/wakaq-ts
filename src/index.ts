83a6304911bd7f5f4fd2464c6536e8f1e3463e58
Alan Hamlett <alan.hamlett@gmail.com>
2023-10-05 11:44:02 +0200

    require using as library not cli

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { ChildWorker } from './childWorker.js';
+export { Scheduler } from './scheduler.js';
+export { inspect, numPendingEtaTasksInQueue, numPendingTasksInQueue, purgeEtaQueue, purgeQueue } from './utils.js';
+export { WakaQ } from './wakaq.js';
+export { WakaWorker } from './worker.js';
